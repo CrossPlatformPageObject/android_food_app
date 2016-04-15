5bf7de10fd3028a6b062d1b55153ea1cd741bd56
priti <priti.biyani6@gmail.com>
2016-04-15 07:48:05 +0530

    Added credit card and purchase summary and radio box event handling

--- app/src/main/java/rajkiranbande/foodoo/MainActivity.java
+++ app/src/main/java/rajkiranbande/foodoo/MainActivity.java
@@ -16,7 +16,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -28,12 +31,16 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener, View.OnClickListener {
+    implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener, View.OnClickListener, RadioGroup.OnCheckedChangeListener {
 
   private static final int FOOD_LIST = 0;
   private static final int FOOD_DETAILS = 1;
   private static final int CART = 2;
   private static final int ADDRESS = 3;
+  private static final int PAYMENT_PREFS = 4;
+  private static final int CHECKOUT = 5;
+  private static final int PURCHASE_SUMMARY = 6;
+  private static final int CREDIT = 7;
   private ListView foodList;
   private ArrayList<FoodItem> foodItems;
   private ArrayAdapter<FoodItem> foodAdapter;
@@ -98,13 +105,19 @@ public class MainActivity extends AppCompatActivity
 
     findViewById(R.id.btnAddToCart).setOnClickListener(this);
     findViewById(R.id.btnCheckout).setOnClickListener(this);
-//    findViewById(R.id.btnProceed).setOnClickListener(this);
+    findViewById(R.id.btnProceed).setOnClickListener(this);
+    findViewById(R.id.btnPaymentPreference).setOnClickListener(this);
+    findViewById(R.id.btnSubmitCreditCard).setOnClickListener(this);
+    ((RadioGroup)findViewById(R.id.rgPayment)).setOnCheckedChangeListener(this);
   }
 
   private void showScreen(int screenIndex) {
     findViewById(R.id.vgFoodList).setVisibility(View.GONE);
     findViewById(R.id.vgFoodItemDetails).setVisibility(View.GONE);
     findViewById(R.id.vgCart).setVisibility(View.GONE);
+    findViewById(R.id.vgAddress).setVisibility(View.GONE);
+    findViewById(R.id.vgPayment).setVisibility(View.GONE);
+    findViewById(R.id.vgCreditCard).setVisibility(View.GONE);
 
     this.screenIndex = screenIndex;
     switch (screenIndex) {
@@ -122,12 +135,26 @@ public class MainActivity extends AppCompatActivity
         cartAdapter.notifyDataSetChanged();
         break;
 
+      case ADDRESS:
+        findViewById(R.id.vgAddress).setVisibility(View.VISIBLE);
+        break;
+
+      case PAYMENT_PREFS:
+        findViewById(R.id.vgPayment).setVisibility(View.VISIBLE);
+        break;
+
+      case CREDIT:
+        findViewById(R.id.vgCreditCard).setVisibility(View.VISIBLE);
+        break;
+
+      case PURCHASE_SUMMARY:
+        findViewById(R.id.vgPurchaseSummary).setVisibility(View.VISIBLE);
     }
   }
 
   @Override
   public void onBackPressed() {
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    DrawerLayout drawer = ( DrawerLayout) findViewById(R.id.drawer_layout);
     if (drawer.isDrawerOpen(GravityCompat.START)) {
       drawer.closeDrawer(GravityCompat.START);
     } else {
@@ -196,8 +223,14 @@ public class MainActivity extends AppCompatActivity
       case R.id.btnAddToCart:
         cartItems.add(selectedFoodItem);
         break;
+      case R.id.btnCheckout:
+        showScreen(ADDRESS);
+        break;
       case R.id.btnProceed:
-
+        showScreen(PAYMENT_PREFS);
+        break;
+      case R.id.btnSubmitCreditCard:
+        showScreen(PURCHASE_SUMMARY);
         break;
     }
   }
@@ -225,6 +258,20 @@ public class MainActivity extends AppCompatActivity
     }
     return total;
   }
+
+  @Override
+  public void onCheckedChanged(RadioGroup group, int checkedId) {
+    switch (checkedId) {
+      case R.id.radio_cash:
+        showScreen(PURCHASE_SUMMARY);
+        break;
+
+      case R.id.radio_credit:
+        showScreen(CREDIT);
+        break;
+    }
+  }
+
 
   public class FoodItemAdapter<F> extends ArrayAdapter<FoodItem> {
     private final Context context;
